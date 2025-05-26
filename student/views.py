ae6a37f9cb87f945ebecf2298c3df343ddb7f291
abishekvp <abirajvp@gmail.com>
2025-05-26 21:11:04 +0530

    d

--- student/views.py
+++ student/views.py
@@ -40,73 +40,12 @@ def library(request, lb=None, dept=None, rack=None, book=None):
         if dept:
             if rack:
                 if book:
-                    if request.method == const.POST:
-                        book_name = str(request.POST.get('book-name')).strip()
-                        author_name = str(request.POST.get('author-name')).strip()
-                        publication_name = str(request.POST.get('publication-name')).strip()
-                        release_date = str(request.POST.get('release-date')).strip()
-                        try:
-                            library = md.Library.objects.get(id=request.POST.get('library_name'))
-                            department = md.Department.objects.get(id=request.POST.get('department_name'))
-                            rack = md.Rack.objects.get(id=request.POST.get('rack_name'))
-                        except md.Library.DoesNotExist:
-                            rack = None
-                            pass
-
-                        book_image = request.FILES.get('book-image')
-                        book = md.Book.objects.get(id=book)
-                        if book_image:
-                            book.image.name = book_image.name
-                            book.image.image = book_image.read()
-                            book.image.extension = book_image.name.split('.')[-1]
-                            book.image.save()
-                        if book_name:
-                            book.name = book_name
-                        if author_name:
-                            book.author = author_name
-                        if publication_name:
-                            book.publication = publication_name
-                        if release_date:
-                            book.release = release_date
-                        if rack:
-                            book.rack = rack
-                        book.save()
-                        return redirect(f'/library/{book.rack.department.library.name}/{book.rack.department.name}/{book.rack.name}/{book.id}')
                     book = md.Book.objects.get(id=book)
                     if book.image:
                         book.base64_image = f"data:image/{book.image.extension};base64," + base64.b64encode(book.image.image).decode('utf-8')
                     else:
                         book.base64_image = None
                     return render(request, 'student/book-detail.html', {'book': book, 'library_name': lb, 'department_name': dept, 'rack_name': rack})
-
-                if request.method == const.POST:
-                    book_name = str(request.POST.get('book-name')).strip()
-                    author_name = str(request.POST.get('author-name')).strip()
-                    publication_name = str(request.POST.get('publication-name')).strip()
-                    release_date = str(request.POST.get('release-date')).strip()
-                    book_file = request.FILES.get('book-file')
-                    if book_file:
-                        book_file = md.File.objects.create(
-                            name=book_file.name,
-                            file=book_file.read(),
-                        )
-                    book_image = request.FILES.get('book-image')
-                    if book_image:
-                        book_image = md.Image.objects.create(
-                            name=book_image.name,
-                            image=book_image.read(),
-                            extension=book_image.name.split('.')[-1]
-                        )
-                    rack_obj = md.Rack.objects.get(department__library__name=lb, department__name=dept, name=rack)
-                    book_obj = dict()
-                    book_obj['name'] = book_name
-                    book_obj['author'] = author_name
-                    book_obj['publication'] = publication_name
-                    book_obj['release'] = release_date
-                    book_obj['file'] = book_file
-                    book_obj['image'] = book_image
-                    book_obj['rack'] = rack_obj
-                    md.Book.objects.create(**book_obj)
                 books = md.Book.objects.filter(rack__department__library__name=lb, rack__department__name=dept, rack__name=rack)
                 for book in books:
                     if book.image:
@@ -114,55 +53,13 @@ def library(request, lb=None, dept=None, rack=None, book=None):
                     else:
                         book.base64_image = None
                 return render(request, 'student/book.html', {'books': books, 'library_name': lb, 'department_name': dept, 'rack_name': rack})
-
-            if request.method == const.POST:
-                rack_name = str(request.POST.get('rack-name')).strip()
-                if not rack_name.isalpha():
-                    messages.warning(request, 'Rack name must contain only alphabetic characters')
-                rack_name = "RK-" + rack_name.upper().replace(' ', '-')
-                if md.Rack.objects.filter(department__library__name=lb, department__name=dept, name=rack_name).exists():
-                    messages.warning(request, 'Rack already exists!')
-                elif rack_name:
-                    department = md.Department.objects.get(name=dept, library__name=lb)
-                    md.Rack.objects.create(name=rack_name, department=department)
-                else:
-                    messages.error(request, 'Unable to create rack!')
             racks = md.Rack.objects.filter(department__library__name=lb, department__name=dept)
             return_data = render(request, 'student/rack.html', {'racks': racks, 'library_name': lb, 'department_name': dept})
             return return_data
-        
-        if request.method == const.POST:
-            department_name = str(request.POST.get('department-name')).strip()
-            if not department_name.isalpha():
-                messages.warning(request, 'Department name must contain only alphabetic characters')
-            department_name = "DEPT-" + department_name.upper().replace(' ', '-')
-            if md.Department.objects.filter(name=department_name, library__name=lb).exists():
-                messages.warning(request, 'Department already exists!')
-            elif department_name:
-                library = md.Library.objects.get(name=lb)
-                md.Department.objects.create(name=department_name, library=library)
-            else:
-                messages.error(request, 'Unable to create department!')
         departments = md.Department.objects.filter(library__name=lb)
         return render(request, 'student/department.html', {'departments': departments, 'library_name': lb})
-    
-    if request.method == const.POST:
-        library_name = str(request.POST.get('library-name')).strip()
-        if not library_name.isalpha():
-            messages.warning(request, 'Library name must contain only alphabetic characters')
-        library_name = "LB-" + library_name.upper().replace(' ', '-')
-        if md.Library.objects.filter(name=library_name).exists():
-            libraries = md.Library.objects.all()
-            messages.warning('Library already exists!')
-            return render(request, 'student/library.html', {'libraries': libraries})
-        elif library_name:
-            md.Library.objects.create(name=library_name)
-        else:
-            messages.error(request, 'Unable to create library!')
     libraries = md.Library.objects.all()
     return render(request, 'student/library.html', {'libraries': libraries})
-
-
 
 # functional units
 @login_required
